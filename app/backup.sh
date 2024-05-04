d58fc79d56d87ca0436624f346161494a9763485
JensOstertag <dev@jens-ostertag.de>
2024-05-04 23:23:11 +0200

    Fix wrong environment variable

--- app/backup.sh
+++ app/backup.sh
@@ -1,4 +1,4 @@
 echo "["$(date +"%Y-%m-%d %H:%M:%S")"] Creating backup..." >> /backup/backup.log 2>&1
 filename="backup-"$(date +"%Y-%m-%d-%H-%M-%S")".sql"
-mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME > /backup/$filename
+mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_DATABASE > /backup/$filename
 echo "["$(date +"%Y-%m-%d %H:%M:%S")"] Backup created: "$filename >> /backup/backup.log 2>&1
