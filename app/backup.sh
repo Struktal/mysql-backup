1fbc09425b2adfe4dc0f75904ab02ebd9524de11
Jens Ostertag <49905418+JensOstertag@users.noreply.github.com>
2025-07-06 13:19:40 +0000

    Fix SSL and file redirection errors

--- app/backup.sh
+++ app/backup.sh
@@ -1,4 +1,4 @@
 echo "["$(date +"%Y-%m-%d %H:%M:%S")"] Creating backup..." >> /backup/backup.log 2>&1
 filename="backup-"$(date +"%Y-%m-%d-%H-%M-%S")".sql"
-mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_DATABASE > /backup/$filename
+mariadb-dump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_DATABASE --skip-ssl > /backup/$filename 2>&1
 echo "["$(date +"%Y-%m-%d %H:%M:%S")"] Backup created: "$filename >> /backup/backup.log 2>&1
